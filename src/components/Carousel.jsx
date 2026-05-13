cd88c373edc0d87607aab57273c3eab2d3df6d88
0xCybri-313 <r.khursheedkhawar@gmail.com>
2026-05-13 23:27:32 +0500

    feat: implement homepage image slider with Swiper API

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -26,20 +26,46 @@ function Carousel() {
     <>
       <Swiper
         slidesPerView={1}
-        spaceBetween={30}
+        spaceBetween={0}
         loop={true}
         pagination={{
           clickable: true,
         }}
         navigation={true}
         modules={[Pagination, Navigation]}
-        className="mySwiper h-128 w-full"
+        className="mySwiper h-196 w-full md:h-144"
       >
-        {[...Array(6)].map((_, index) => (
-          <SwiperSlide key={index} className="bg-gray-400 pt-12 text-center">
-            <span className="text-2xl font-semibold">Slide {index + 1}</span>
-          </SwiperSlide>
-        ))}
+        {data &&
+          data.slice(0, 5).map((item, index) => (
+            <SwiperSlide
+              key={index}
+              className="flex h-full w-full items-center justify-center bg-linear-to-r from-[#0f0c29] via-[#302b63] to-[#24243e] text-center"
+            >
+              <div className="my-20 flex h-144 w-full flex-col items-center justify-center gap-10 px-4 md:my-0 md:flex-row">
+                <div className="space-y-3 md:space-y-6">
+                  <h3 className="font-sans text-sm font-semibold text-red-500">
+                    Powering Your World with the Best in Electronics
+                  </h3>
+                  <h1 className="line-clamp-2 text-xl font-bold text-white uppercase md:line-clamp-3 md:w-125 md:text-4xl">
+                    {item.title}
+                  </h1>
+                  <p className="line-clamp-3 pr-7 text-gray-400 md:w-125">
+                    {item.description}
+                  </p>
+                  <button className="mt-2 cursor-pointer rounded-md bg-linear-to-r from-red-500 to-purple-500 px-3 py-2 text-white">
+                    Shop Now
+                  </button>
+                </div>
+                <div>
+                  <img
+                    src={item.images}
+                    alt={item.title}
+                    className="w-md rounded-full shadow-2xl shadow-red-400 transition-all hover:scale-105"
+                  />
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
       </Swiper>
     </>
   );
